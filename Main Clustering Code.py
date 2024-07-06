aff0313095558bb2b8493f92990502ccadaf0f7c
CasperClarke <CameronClarke@verizon.net>
2024-07-06 14:15:50 -0500

    Added printouts for confidence

--- Main Clustering Code.py
+++ Main Clustering Code.py
@@ -177,7 +177,7 @@ def FindMutualInformation(truelist, clusterlist): # set up for 3 clusters, fix l
 
 # Find the distibution of wins for each algorithm with a given partition list of form:
 # [1, 5, 2, 6], where each element is the amount of items in that partition
-def FindBestPerformer(partition_list, num_samples, caseName, confidence):
+def FindBestPerformer(partition_list, num_samples, caseName):
     truth_list = generate_list(partition_list)
     linkagePerformance = [[] for x in linkagenames]
     linkageTimes = {name:0 for name in linkagenames}
@@ -199,7 +199,9 @@ def FindBestPerformer(partition_list, num_samples, caseName, confidence):
     plt.figure(caseName)
     performance = [sum(linkageType)/num_samples for linkageType in linkagePerformance]
     standardDeviation = [np.std(linkageType)/np.sqrt(num_samples) for linkageType in linkagePerformance]
-    
+    print("\n",caseName)
+    for i in range(len(linkagenames)):            
+        print("Linkage Method:",linkagenames[i],"    \t Mean:",performance[i],"\t Confidence Interval:",standardDeviation[i])
     plt.errorbar(linkagenames, performance, yerr=standardDeviation, fmt='o', color='k',ecolor = "black", capsize=3)
     plt.xlabel('Linkage Method')
     plt.ylabel('Average Adjusted Mutual Information Score')
@@ -219,13 +221,13 @@ def FindBestPerformer(partition_list, num_samples, caseName, confidence):
 # partitions!! :P
 
 partition_even = [3, 3, 3, 3, 3, 3]
-timesTaken = FindBestPerformer(partition_even,1000,"Partition Even", 0.95)
+timesTaken = FindBestPerformer(partition_even,10000,"Partition Even")
 print(timesTaken)
 partition_uneven = [1, 3, 9, 7, 5, 11]
-FindBestPerformer(partition_uneven,1000,"Partition Uneven", 0.95)
+FindBestPerformer(partition_uneven,10000,"Partition Uneven")
 
 partition_small_uneven = [2, 3, 1, 4]
-FindBestPerformer(partition_small_uneven,1000,"Partition Small Uneven", 0.95)
+FindBestPerformer(partition_small_uneven,10000,"Partition Small Uneven")
 
 def getComputationTime(num_samples, figureName, minSize, maxSize, stepSize):
     countValues = []
